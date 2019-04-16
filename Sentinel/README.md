778c68aea75e5a0a7e9d9253f49be3c74ae93524
xxm <xxm@163.com>
2019-04-16 15:22:22 +0800

    添加gateway路由和Sentinel熔断仪表盘监控

---
+++ Sentinel/README.md
@@ -0,0 +1,7 @@
+# sentinel-dashboard
+```
+cd sentinel-dashboard/target/
+
+java -Dserver.port=8080 -jar sentinel-dashboard.jar
+或者使用  启动脚本.cmd
+```
--- spring-cloud-alibaba-nacos-provider/src/main/java/com.xxm.nacos/provider/NacosProviderApplication.java
+++ spring-cloud-alibaba-nacos-provider/src/main/java/com.xxm.nacos/provider/NacosProviderApplication.java
@@ -21,7 +21,6 @@ public class NacosProviderApplication {
 
 
 
-
     @RestController
     public class EchoController {
         @GetMapping(value = "/echo/{message}")
