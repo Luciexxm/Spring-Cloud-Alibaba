9106f7125cbd6915f04b97e07c0468b670793f4e
小小莫 <47232774+Luciexxm@users.noreply.github.com>
2020-07-09 14:42:09 +0800

    Update FileUpload.java

--- xxm-oss/src/main/java/com/xxm/oss/FileUpload.java
+++ xxm-oss/src/main/java/com/xxm/oss/FileUpload.java
@@ -8,8 +8,8 @@ public class FileUpload {
 
     public static void upload() {
         String endpoint = "http://oss-cn-shenzhen.aliyuncs.com";
-        String accessKeyId = "LTAICeP1kEoAe69n";
-        String accessKeySecret = "42wxZfHrdJuWWrFkQfkV7LolCfkbP0";
+        String accessKeyId = "******";
+        String accessKeySecret = "******";
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
 
         ossClient.putObject("xxm-oss", "xxm-oss", new File("C:/Users/mmx/Pictures/33.jpg"));
